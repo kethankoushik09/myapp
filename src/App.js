f92b0c3b15647826f7969b59aec578045254a55a
Kethan Koushik <kethankoushik09@gmail.com>
2025-05-21 15:12:14 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-// src/App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AuthPage from './components/AuthPage';
-import Signup from './components/Signup';
-import Login from './components/Login';
-import ForgotPassword from './components/ForgotPassword';
+// // src/App.jsx
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import AuthPage from './components/AuthPage';
+// import Signup from './components/Signup';
+// import Login from './components/Login';
+// import ForgotPassword from './components/ForgotPassword';
 
-import MainPage from './components/MainPage';
+// import MainPage from './components/MainPage';
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<AuthPage />} />
+//         <Route path="/signup" element={<Signup />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/forgot-password" element={<ForgotPassword />} />
+
+//         <Route path="/main" element={<MainPage />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<AuthPage />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-
-        <Route path="/main" element={<MainPage />} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>Hello, World!</h1>
+      <p>Welcome to my deployed React app on GitHub Pages!</p>
+    </div>
   );
-};
-
+}
 export default App;
